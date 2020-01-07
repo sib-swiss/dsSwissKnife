7cf63bc0df2a37a4ba1026a85c4851fcce2a69d4
Iulian <iulian_dragan@yahoo.com>
2020-01-07 10:23:44 +0100

    partCov

--- R/little_helpers.R
+++ R/little_helpers.R
@@ -23,3 +23,24 @@
   nums <- nums[!(names(nums) %in% hidden)] # no SUBJID
   list(numerics = x[,names(nums), drop= FALSE], others = x[,!(colnames(x) %in% names(nums)), drop = FALSE])
 }
+
+#' @title Decode from base64 and deserialize from json if necessary
+#' @description Work around the restrictions imposed by the Opal server on function arguments
+#' The Opal server is very picky as regards function arguments. The workaround is
+#' to serialize and encode them on the client and strip the right padding.
+#' @details It looks for the string 'base64' in the argument to determine if it's encoded
+#' @param some.thing the thing to be decoded and deserialized from json if necessary
+#' @return the decoded and deserialized argument
+#'
+.decode.arg <- function(some.thing, simplifyMatrix = FALSE){
+
+  if(length(some.thing) == 1 && grepl('base64', some.thing, ignore.case = TRUE)){
+    some.thing <- gsub('base64', '', some.thing, ignore.case =TRUE)
+    my.dictionary = c('-plus-' = '+', '-slash-' = '/', '-equals-' = '=')
+    sapply(names(my.dictionary), function(x){
+      some.thing <<- gsub(x, my.dictionary[x], some.thing)
+    })
+    some.thing <- jsonlite::fromJSON(RCurl::base64Decode(some.thing), simplifyMatrix = simplifyMatrix)
+  }
+  return(some.thing)
+}
---
+++ R/partCov.R
@@ -0,0 +1,59 @@
+#'
+#' @title calculates the matrix of sums of products of distance to the global means,
+#' @description used as a piece of the global covariance matrix
+#' the client sums all these local matrices and divides them by N-1 -> cov matrix
+#' @param x a data frame of matrix
+#' @param means a vector of global means (one for each dimension)
+#' @param na.rm logical remove NAs?
+#' @param collist a vector, subset of columns to use
+#' @return  a matrix of the sums of products for all pairs of dimensions
+
+#' @export
+#'
+
+
+partCov <- function(x, means = NULL,  collist = NULL){
+  # returns a matrix of
+  #   sums of
+  #     products (for each pair of dimensions) of
+  #       distances to the global mean (for each dimension)
+  # the client will sum these and divide by the respective, global, N-1
+  # the client ends up with a global covariance matrix
+
+  if (is.data.frame(x)){
+
+    if(!is.null(collist)){
+      collist <- .decode.arg(collist)
+    }
+    y <- .split.numerics(x, collist)$numerics
+
+    z <- as.matrix(y)
+  } else if(is.matrix(x)) {
+    z <- x
+  } else {
+    stop("'x' must be a matrix or a data frame")
+  }
+  # no NAs
+  if(anyNA(z)){
+    stop("'x' cannot contain NAs")
+  }
+  # must be datashield valid (more than <datashield.privacyLevel> rows):
+  if(!dsBase::isValidDS(x)){
+    stop(paste0('only ', nrow(x), ' rows'))
+  }
+  means <- .decode.arg(means)
+
+  if(is.null(means) | length(means) == 0){
+    # no global means, we return the local term
+    means <- colMeans(z)
+  }
+
+  if (length(means) != ncol(z)){
+    stop("length of the means vector must equal the number of columns in 'x'")
+  }
+
+  # subtract the means (we know from above that the dimensions match):
+  centered <- sweep(z, 2, means, '-', check.margin = FALSE)
+  # go forth and multiply:
+  crossprod(centered)
+}
