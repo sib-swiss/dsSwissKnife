5a36376e608311c0e2c62bf8aa06ba1d7d3676a1
Iulian <iulian_dragan@yahoo.com>
2020-01-13 12:10:41 +0100

    subsetByClass

--- R/little_helpers.R
+++ R/little_helpers.R
@@ -76,3 +76,25 @@
 
   return(output)
 }
+
+
+.dsbase_extract <- function (input){
+  input <- unlist(input)
+  output1 <- c()
+  output2 <- c()
+  for (i in 1:length(input)) {
+    inputterms <- unlist(strsplit(input[i], "\\$", perl = TRUE))
+    if (length(inputterms) > 1) {
+      obj1 <- strsplit(input[i], "\\$", perl = TRUE)[[1]][1]
+      obj2 <- strsplit(input[i], "\\$", perl = TRUE)[[1]][2]
+    }
+    else {
+      obj1 <- NA
+      obj2 <- strsplit(input[i], "\\$", perl = TRUE)[[1]][1]
+    }
+    output1 <- append(output1, obj1)
+    output2 <- append(output2, obj2)
+  }
+  output <- list(holders = output1, elements = output2)
+  return(output)
+}
---
+++ R/subsetByClass.R
@@ -0,0 +1,90 @@
+subsetByClass <- function(first.arg, cols, keep.cols = TRUE){
+  first.arg <- .decode.arg(first.arg)
+  cols <- .decode.arg(cols)
+
+  objs <- .dsbase_extract(cols)
+  call_env <- parent.frame()
+  categories <- data.frame(Map(function(x, y){
+    if(is.na(y)){
+      myenv <- call_env
+    } else {
+      theenv <- call_env
+      myenv <- as.environment(get(y, envir = theenv))
+    }
+    get(x, myenv)
+  }, objs$elements, objs$holders))
+
+
+
+  #  real.args <- sapply(first.arg, function(x){
+  #    ret <-  get(x, envir = .GlobalEnv)
+
+  #    if(!is.data.frame(ret)){
+  #      tmp <- list()
+  #      tmp[[x]] <- ret
+  #      ret <- tmp
+  #    }
+  #    ret
+  #    }, simplify = FALSE)
+
+  argobjs <- .dsbase_extract(first.arg)
+
+  real.args<- Map(function(x, y){
+    if(is.na(y)){
+      myenv <- call_env
+      ret <- get(x, myenv)
+    } else {
+      theenv <- call_env
+      myenv <- as.environment(get(y, envir = theenv))
+      ret <- get(x, myenv)
+    }
+
+    if(!is.data.frame(ret)){
+      tmp <- list()
+      tmp[[x]] <- ret
+      ret <- tmp
+    }
+    ret
+  }, argobjs$elements, argobjs$holders)
+
+
+
+
+  real.args.df <- data.frame(real.args)
+
+
+  colnames(real.args.df) <- unlist(sapply(real.args, names, simplify = FALSE))
+
+  categories <- categories[,!(names(categories) %in% names(real.args.df) ),drop = FALSE]
+
+  real.args.df <- cbind(real.args.df, categories , deparse.level = 2)
+  .subsetByClass.df(real.args.df, cols = objs$elements, keep.cols = keep.cols)
+}
+
+
+
+.subsetByClass.df <- function(df, cols, keep.cols = TRUE){
+  #df <- get(data, envir = .GlobalEnv)
+  # must be factors. If any of the cols isn't, complain.
+  if(!all(sapply(df[, cols], is.factor))){
+    stop('The subset column must be a factor.')
+  }
+
+  ret <- split(df[,keep.cols, drop = FALSE], df[,cols])
+
+  names(ret) <- make.names(names(ret))
+  out <- lapply(ret, function(x){
+    if (nrow(x) < dsBase:::setFilterDS()){
+      return(NULL)
+    }
+    as.data.frame(lapply(x, function(y){ # reset the factor levels
+      if(is.factor(y)){
+        return(factor(y))
+      }
+      y
+    }))
+
+  })
+  #return only the meaningful part:
+  out[!sapply(out, is.null)]
+}
