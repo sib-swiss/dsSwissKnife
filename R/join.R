0bca2613a9aaa67acdbadbf500f8c64e00af0e46
Iulian <iulian_dragan@yahoo.com>
2020-01-16 13:18:32 +0100

    join, safeSubset

---
+++ R/join.R
@@ -0,0 +1,32 @@
+#' @export
+join <- function(what, type='full', bycol = NULL){
+
+  bycol <- .decode.arg(bycol)
+  if(is.null(bycol)){
+    tryCatch(bycol <- get('by.col', envir = .mycache), error = function(e){stop('Run init first')})
+  } else {
+    bycol <- unlist(bycol)
+  }
+  where <- parent.frame()
+  what <- lapply(.decode.arg(what), function(x) get(x, envir = where))
+
+  if (type == 'full'){
+    ret <- Reduce(function(x,y) dplyr::full_join(x,y, by = bycol), what )
+  }
+  if (type == 'inner'){
+    ret <- Reduce(function(x,y) dplyr::inner_join(x,y, by = bycol), what )
+  }
+  if (type == 'left'){
+    ret <- Reduce(function(x,y) dplyr::left_join(x,y, by = bycol), what)
+  }
+  if (type == 'right'){
+    ret <- Reduce(function(x,y) dplyr::right_join(x,y, by = bycol), what )
+  }
+  if (type == 'semi'){
+    ret <- Reduce(function(x,y) dplyr::semi_join(x,y, by = bycol), what )
+  }
+  if (type == 'anti'){
+    ret <- Reduce(function(x,y) dplyr::anti_join(x,y, by = bycol), what)
+  }
+  ret
+}
---
+++ R/safeSubset.R
@@ -0,0 +1,55 @@
+#' @title subset a dataframe
+#' @description takes potentially unsafe row and column filters and evals them in the context of the dataframe.
+#' It takes the following precautions:
+#' * it takes a snapshot of all the envirnoments before the execution and deletes any new objects it finds after the execution
+#' * it locks all environments before the execution, so no existing object can be modified
+#' @param df a dataframe
+#' @param row.filter a character, the filter for rows (as in '[')
+#' @param col.filter a character, the filter for columns (as in '[')
+#' @export
+
+
+safeSubset <- function(df, row.filter = TRUE, col.filter = TRUE){
+
+  stopifnot(is.data.frame(df))
+  row.filter <- .decode.arg(row.filter)
+  col.filter <- .decode.arg(col.filter)
+  if(is.null(c(row.filter, col.filter))){
+    return(df)
+  }
+  # take a snapshot of the current session
+  safe.objs <- .ls.all()
+  # lock everything so no objects can be changed
+  .lock.unlock(safe.objs, lockBinding)
+  tryCatch({
+    hidden <-  get('hidden', envir = .mycache)
+    #removing this restriction for now:
+    #mycols <- setdiff(colnames(df), hidden)
+    #ret <-  df[eval(parse(text = row.filter), envir = df[,mycols], enclos = .GlobalEnv), eval(parse(text = col.filter), envir = df[,mycols], enclos = .GlobalEnv), drop = FALSE]
+    ret <-  df[eval(parse(text = row.filter), envir = df, enclos = .GlobalEnv), eval(parse(text = col.filter), envir = df, enclos = .GlobalEnv), drop = FALSE]
+    # reform the factors with the new levels:
+    ret <- as.data.frame(lapply(ret, function(x){
+      if (class(x) == 'factor'){
+        return(factor(x))
+      }
+      x
+    }))
+
+  },
+  error = function(e){
+    # if anything happens, unlock and cleanup:
+    .lock.unlock(safe.objs, unlockBinding)
+    .cleanup(safe.objs)
+    stop(e)
+  })
+  # unlock back everything
+  .lock.unlock(safe.objs, unlockBinding)
+  # get rid of any sneaky objects that might have been created in the filters as side effects
+  .cleanup(safe.objs)
+  # basic check:
+  if(!dsBase::isValidDS(ret)){
+    ret[] <- NA
+  }
+  ret
+}
+
