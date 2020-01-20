290f9292db88fe76a1a6c375026e21daa7f49700
Iulian <iulian_dragan@yahoo.com>
2020-01-20 16:49:34 +0100

    cutDS, rbindDS

---
+++ R/cutDS.R
@@ -0,0 +1,27 @@
+#' @export
+cutDS <- function(arglist){
+
+  arglist <- .decode.arg(arglist)
+
+  if(arglist[['in.place']]){
+    new.name <- arglist$x
+    arglist$in.place <- NULL
+  } else {
+    new.name <- arglist$new.name
+  }
+
+  arglist$new.name <- NULL
+  if(!is.null(arglist$df)){
+    retname <- arglist$df
+    mydf <- get(retname, envir = parent.frame())
+    arglist$df <- NULL
+    arglist$x <- mydf[,arglist$x]
+    mydf[, new.name] <- do.call(cut, arglist)
+    assign(retname, mydf, envir = parent.frame())
+  } else {
+    retname <- new.name
+    arglist$x <- get(arglist$x, envir = parent.frame())
+    assign(retname, do.call(cut, arglist), envir = parent.frame())
+  }
+  return(retname)
+}
---
+++ R/rbindDS.R
@@ -0,0 +1,14 @@
+#' @export
+rbindDS <- function(x,y, new.colnames = NULL){
+  new.colnames <- .decode.arg(new.colnames)
+  if (!is.null(new.colnames)){
+    colnames(x) <- new.colnames
+    colnames(y) <- new.colnames
+  }
+
+  ret <- rbind(x,y)
+  if(!dsBase::isValidDS(ret)){
+    ret <- ret[NA, NA]
+  }
+  ret
+}
