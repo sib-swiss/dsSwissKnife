1c1f894019478e25f24254b829373562756544ef
Iulian <iulian_dragan@yahoo.com>
2020-02-06 09:48:58 +0100

    selfUpgrade

---
+++ R/selfUpgrade.R
@@ -0,0 +1,24 @@
+#' @export
+selfUpgrade <- function(other.package = NULL ,method = NULL, lib = NULL, verbose = FALSE , repos = ){
+  # systemd sometimes goes and deletes the rserv tempdir, we need to recreate it if necessary
+  tmp <- tempdir()
+  if(!dir.exists(tmp)){
+    dir.create(tmp)
+  }
+  if(!is.null(lib)){
+    lib = .decode.arg(lib)
+  }
+  x <- list( dsCDISC = capture.output(install.packages('dsSwissKnife', lib = lib, repos=c('https://rhap-fdb01.vital-it.ch/repo', 'http://rhap-fdb01.vital-it.ch/repo', 'https://stat.ethz.ch/CRAN', 'http://stat.ethz.ch/CRAN'),
+                                                       method = method), type = c('message')))
+  if(!is.null(other.package)){
+    x[[other.package]] <- capture.output(install.packages(other.package, lib = lib,
+                                                          repos=c('https://rhap-fdb01.vital-it.ch/repo', 'http://rhap-fdb01.vital-it.ch/repo', 'https://stat.ethz.ch/CRAN', 'http://stat.ethz.ch/CRAN'), method = method),type = c('message'))
+  }
+  if(verbose){
+    x[['installed']] <- installed.packages()
+    x[['libs']] <- .libPaths()
+    x[['mem']] <- system('free -m', intern = TRUE)
+  }
+  return(x)
+
+}
