1a5dedde8f237beffddb1b46ad08074b97d89340
Iulian <iulian_dragan@yahoo.com>
2020-06-23 15:35:35 +0200

    HmiscDSS

---
+++ R/HmiscDSS.R
@@ -0,0 +1,24 @@
+HmiscDSS <- function(func, arglist, newobj){
+  myenv <- parent.frame()
+  force(myenv)
+  dispatcher <- list(
+    rcorrcens= function(...) {
+      a <- list(...)
+      a[[1]] <- as.formula(a[[1]])
+      names(a)[1] <- 'formula'
+      if('data' %in% names(a)){
+        a[['data']] <- get(a[['data']], envir = myenv)
+      }
+
+    do.call(Hmisc:::rcorrcens.formula,a)
+
+  }
+ )
+  func <- .decode.arg(func)
+  arglist <- .decode.arg(arglist)
+  newobj <- .decode.arg(newobj)
+  if(!(func %in% names(dispatcher))){
+    stop(paste0(func, ' not implemented yet.'))
+  }
+  do.call(dispatcher[[func]], arglist)
+}
