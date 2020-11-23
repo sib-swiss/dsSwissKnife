d05a8593b7ba102e3192bd3f16c6a601fa6996f1
IulianD <iulian_dragan@yahoo.com>
2020-11-23 14:25:02 +0000

    fixed makeFactorsDSS

--- R/makeFactorsDSS.R
+++ R/makeFactorsDSS.R
@@ -1,5 +1,4 @@
 makeFactorsDSS <- function(df){
-  df <- get(df, envir = parent.frame())
   as.data.frame(sapply(df, function(y){
     if(length(intersect(class(y) , c('character', 'Date', 'POSIXct', 'POSIXlt', 'POSIXt'))) >0 ){
       return(factor(y))
