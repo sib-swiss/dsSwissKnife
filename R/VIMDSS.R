00c3fa519a8183ba425840375acb1c661c4d28cc
IulianD <iulian_dragan@yahoo.com>
2021-01-27 10:14:46 +0000

    fixed VIMDSS

--- R/VIMDSS.R
+++ R/VIMDSS.R
@@ -1,14 +1,13 @@
 VIMDSS <- function(func, arglist, newobj = NULL){
   myparent <- parent.frame()
 
- .kNN <- function(newobj, data, ...){
-  df <- get(data, envir = myparent)
+ .kNN <- function(newobj, ...){
   if(is.null(newobj)){
-    newobj <- data
+    newobj <- arglist$x
   }
   myargs <- list(...)
   # get rid of SUBJID in the imputation:
-  dfcols <- colnames(df)
+  dfcols <- colnames(myargs$x)
   hidden <- get('hidden', envir = .mycache)
   sapply(c('variable', 'dist_var'), function(p){
     if(is.null(myargs[[p]])){
@@ -16,17 +15,17 @@ VIMDSS <- function(func, arglist, newobj = NULL){
     }
     myargs[[p]] <<- setdiff(myargs[[p]], hidden)
   })
-  myargs$data <- df
+  names(myargs)[names(myargs)=='x'] <- 'data'
   out <- do.call(VIM::kNN, myargs)
   assign(newobj, out, envir = myparent)
   return(TRUE)
  }
 
- .aggr <- function( x, ...){
-  df <- get(x, envir =  myparent)
+ .aggr <- function(...){
+
   png('temp.png', width = 1024, height = 768)
   #out <- VIM::aggr(df, plot = FALSE)
-  out <- VIM::aggr(df,...)
+  out <- VIM::aggr(...)
   #png('temp.png', width = 960, height = 960)
   #myargs <- list(...)
   #myargs$x <- out
@@ -42,13 +41,29 @@ VIMDSS <- function(func, arglist, newobj = NULL){
    kNN = function(...) .kNN(newobj,...),
    aggr = .aggr
  )
+
  func <- .decode.arg(func)
  arglist <- .decode.arg(arglist)
+
  newobj <- .decode.arg(newobj)
+ # replace the name of the data frame with itself:
+ names(arglist)[names(arglist)=='data'] <- 'x'
+ newArglist <- arglist
+ if('x' %in% names(arglist) ){
+   newArglist$x <- get(arglist$x, envir = myparent)
+ } else {
+   newArglist[[1]] <- get(arglist[[1]], envir = myparent )
+   names(newArglist)[1] <- names(arglist)[1] <- 'x'
+
+ }
+
  if(!(func %in% names(dispatcher))){
    stop(paste0(func, ' not implemented yet.'))
  }
- do.call(dispatcher[[func]], arglist)
+
+ do.call(dispatcher[[func]], newArglist)
 
 
 }
+
+
