91f81399cda84914434bd63dc201dee1d7be6303
IulianD <iulian_dragan@yahoo.com>
2021-08-25 10:16:23 +0200

    egfr in deriveColumn

--- R/deriveColumn.R
+++ R/deriveColumn.R
@@ -30,5 +30,30 @@ deriveColumn <- function(x, colname, formula){
   return(parsed)
 }
 
+# egfr for Andreas Heinzel (beatdkd)
+
+egfr <- function(scr, sex, age, black = FALSE) {
+  if(length(scr) > 1){
+    stop('Only one measurement of creatinine allowed.')
+  }
+  if(tolower(sex) %in% c('m', 'male') ){
+    alfa <- -0.411
+    k <- 0.9
+    coef1 <-  1
+  } else if (tolower(sex) %in% c('f', 'female')){
+    alfa <- -0.329
+    k <- 0.7
+    coef1 <- 1.018
+  }
+
+  coef2 <- 1
+  if(black){
+    coef2 <- 1.159
+  }
+
+  return(141 * (min(scr/k, 1))^alfa * (max(scr/k, 1))^(-1.209) * 0.993^age * coef1 * coef2)
+
+}
+
 
 
