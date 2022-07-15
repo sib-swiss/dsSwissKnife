76269084fefb1de3ab3bfda634df20b5b51d71f7
IulianD <iulian_dragan@yahoo.com>
2022-07-15 15:01:17 +0200

    uwot half way

--- R/uwotDSS.R
+++ R/uwotDSS.R
@@ -1,9 +1,22 @@
-uwotDSS <- function(func, X, arglist){
+uwotDSS <- function(func, X, arglist = list(), model = NULL){
+  if(!(func %in% c('umap', 'umap_transform'))){
+    stop(paste0(func, ' not implemented.'))
+  }
   X <- .split.numerics(X)$numerics
   arglist <- .decode.arg(arglist)
   arglist$X <- X
-  do.call(eval(parse(text=paste0('uwot::', func))), arglist)
+  # some sanitizing:
+  arglist$ret_nn = FALSE
+  arglist$ret_extra = c()
 
+  if(!is.null(model)){ # this is a path to the saved model
+    arglist$model <- uwot::load_uwot(model)
+  }
+  res <- do.call(eval(parse(text=paste0('uwot::', func))), arglist)
+  fname <- paste0(getwd(),'uwotDSS_model')
+
+  uwot::save_uwot(res, fname, unload = TRUE)
+  return(fname)
 }
 
 
