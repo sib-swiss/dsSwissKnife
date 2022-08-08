223bacff806df43a731e743585b6c3c5ef36e499
IulianD <iulian_dragan@yahoo.com>
2022-08-08 16:36:12 +0200

    fix uwotDSS

--- R/uwotDSS.R
+++ R/uwotDSS.R
@@ -12,11 +12,16 @@ uwotDSS <- function(func, X, arglist = list(), model = NULL){
   if(!is.null(model)){ # this is a path to the saved model
     arglist$model <- uwot::load_uwot(model)
   }
-  res <- do.call(eval(parse(text=paste0('uwot::', func))), arglist)
-  fname <- paste0(getwd(),'uwotDSS_model')
+  mod <- do.call(eval(parse(text=paste0('uwot::', func))), arglist)
+  fname <- tempfile(pattern='uwotDSS', tmpdir = tempdir(check=TRUE))
 
-  uwot::save_uwot(res, fname, unload = TRUE)
-  return(fname)
+  uwot::save_uwot(mod, fname, unload = TRUE)
+  # get the size in order to read it back
+  sz <- file.info(fname)$size
+  # read and return it
+  res  <- readBin(fname, 'raw', n=sz)
+#  return(fname)
+  return(res)
 }
 
 
