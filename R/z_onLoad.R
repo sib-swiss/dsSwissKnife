e307999251073af8f2f92fe24c56e9815a8ea1aa
IulianD <iulian_dragan@yahoo.com>
2022-10-03 14:09:34 +0200

    new options for datashield 6.2

--- R/z_onLoad.R
+++ R/z_onLoad.R
@@ -3,5 +3,9 @@
   options('hidden.fields.regexes' = c('source'))
   options('hidden.fields' = c('person_id'))
   options('join.pivot.col' = c('person_id'))
+  # and some more for datashield 6.2
+  options('datashield.privacyControlLevel'  = 'permissive')
+  options('default.nfilter.levels.density' = 0.33)
+  options('default.nfilter.levels.max' = 40)
   .init()
 }
