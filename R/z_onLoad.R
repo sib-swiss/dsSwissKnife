11fd798ce96b5b64ae666082e02a833696abbcfd
IulianD <iulian_dragan@yahoo.com>
2022-12-06 08:54:35 +0100

    no more option hidden fields

--- R/z_onLoad.R
+++ R/z_onLoad.R
@@ -1,6 +1,6 @@
 .onLoad <- function(...){
   # set some options here temporarily, remove them once the admins set them in opal:
-  options('hidden.fields.regexes' = c('source'))
+
   options('hidden.fields' = c('person_id'))
   options('join.pivot.col' = c('person_id'))
   # and some more for datashield 6.2
